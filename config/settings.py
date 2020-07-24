f1e90ef05790a8c4576f106fe12161c0214bfb4d
alexjeman <contact@alexjeman.com>
2020-07-24 14:58:56 +0300

    option mail TLS

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,8 @@ class Settings(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = os.getenv('MAIL_PORT')
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL')
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_DEBUG = DEBUG
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
